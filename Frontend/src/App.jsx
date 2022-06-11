a8e7e40c62702cf89027dbae60b41c3479eeb91b
Won jonghyeok <whdgur3077@hanmail.net>
2022-06-11 15:12:28 +0900

    :tada: 프로젝트생성

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Register from "./Layout/register/register";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Register />
+    </>
+  );
+};
+
+export default App;
