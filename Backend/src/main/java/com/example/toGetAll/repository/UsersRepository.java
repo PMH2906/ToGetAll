8441fc1414ea94ceab7282bf5e9ec5c6230afd7d
MIHEE PARK <44748142+PMH2906@users.noreply.github.com>
2022-06-25 16:24:40 +0900

    Delete Backend directory

--- Backend/src/main/java/com/example/toGetAll/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.toGetAll.repository;
-
-import com.example.toGetAll.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
